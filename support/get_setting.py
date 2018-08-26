10c5d7f62620eb633332b6737c166bcdd226a0ca
Frederich Munch <marsupial@users.noreply.github.com>
2018-08-26 15:41:40 -0700

    Squashed 'sublime/CMakeBuilder/' content from commit 171dc987
    git-subtree-dir: sublime/CMakeBuilder
    git-subtree-split: 171dc9874462fa435b1b54ff3504f0104e87298d
    git-subtree-repo: https://github.com/rwols/CMakeBuilder.git

---
+++ support/get_setting.py
@@ -0,0 +1,10 @@
+import sublime
+
+
+def get_setting(view, key, default=None):
+    if view:
+        settings = view.settings()
+        if settings.has(key):
+            return settings.get(key)
+    settings = sublime.load_settings('CMakeBuilder.sublime-settings')
+    return settings.get(key, default)
